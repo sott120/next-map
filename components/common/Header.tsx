febfe14544ca8f43429cf8f79a83280d388a6772
soyeon park <psy_sott120@naver.com>
2024-12-10 23:05:45 +0900

    feat: styled component 추가

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '@/styles/header.module.scss';
 import styled from 'styled-components';
+import Image from 'next/image';
 
 interface Props {}
+const logo = '/logo.png';
 
 export default function HeaderComponent({}: Props) {
   return (
     <S.Header>
       <div className="header">
         <Link href="/">
-          <img src="/logo.png" alt="헤더로고" />
+          <div className="header__logo">
+            <Image className="header__logo--image" src={logo} fill sizes="100" alt="헤더로고" priority={true} />
+          </div>
         </Link>
+        <div className="header__icons">아이콘 영역</div>
       </div>
     </S.Header>
   );
@@ -19,6 +23,25 @@ export default function HeaderComponent({}: Props) {
 
 const S = {
   Header: styled.header`
-    background-color: gray;
+    background-color: #d7d7d7;
+
+    .header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      z-index: 100;
+      pointer-events: none;
+      &__logo {
+        display: block;
+        position: relative;
+        width: 100px;
+        height: 50px;
+        pointer-events: auto;
+        &--image {
+          position: relative;
+          object-fit: cover;
+        }
+      }
+    }
   `,
 };
